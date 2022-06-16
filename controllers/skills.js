053c4fd88821cf6f3856322b28bdcd16e80f15b5
dlobera <delobera@gmail.com>
2022-06-16 15:40:53 -0500

    add skills

---
+++ controllers/skills.js
@@ -0,0 +1,13 @@
+import { skills } from "../data/skills.js"
+
+function index (req, res) {
+  res.render ('skills/index', {
+    skills : skills
+  })
+}
+
+
+
+export {
+  index
+}
---
+++ data/skills.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'HTML', done: true},
+  {text: 'CSS', done: true},
+  {text: 'Javascript', done: false},
+  {text: 'Mongoose DB', done: false},
+  {text: 'Express', done: false},
+  {text: 'Node', done: false},
+]
+
+export { 
+	skills
+}
