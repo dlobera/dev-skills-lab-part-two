c113945b38d9e605978499fbf44dbf9fd3f5c3d7
dlobera <delobera@gmail.com>
2022-06-16 21:57:11 -0500

    add delete button

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,17 +1,5 @@
 import { Skill } from "../models/skill.js"
 
-function index (req, res) {
-  Skill.find({})
-  .then(skills => {
-    res.render ('skills/index', {
-      skills : skills
-    })
-  })
-  .catch(error => {
-    console.log(error)
-    res.redirect('/')
-  })
-}
 
 function newSkill(req, res) {
   res.render('skills/new')
@@ -42,11 +30,36 @@ function show (req, res) {
   })
   }
 
+function index(req, res) {
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills : skills,
+      time: req.time
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
 
 
 export {
   index,
   newSkill as new,
   create, 
-  show
+  show,
+  deleteSkill as delete,
 }
